26c0bee43e5f38178fad289295264733da7e0d3e
Devansh Chandak <dchandak99@gmail.com>
2019-11-03 11:14:52 +0530

    chage password done

---
+++ mysite/core/views.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserChangeForm
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'change_password.html', {
+        'form': form
+    })
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form=UserChangeForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    else:
+        form = UserChangeForm()
+    return render(request, 'edit_profile.html', {'form': form})
---
+++ mysite/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
+from mysite.core import views as core_views
+
+
+urlpatterns = [
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'^password/$',core_views.change_password, name='change_password'),
+    #url('^change_password/$', auth_views.password_change, {'post_change_redirect': 'next_page'}, name='password_change'),
+    url(r'^edit_profile/$',core_views.edit_profile, name='edit_profile'),
+]
