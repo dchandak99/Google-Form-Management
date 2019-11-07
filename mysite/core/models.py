cc2e1db68d9eae26611345de2b9e09f997d3c82a
Devansh Chandak <dchandak99@gmail.com>
2019-11-07 11:39:27 +0530

    Data2 complete

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -10,8 +10,8 @@ class Data1(models.Model):
     Answer3= models.CharField(max_length=200,default='Hi')
     
 class Data2(models.Model):
-     client= models.CharField(max_length=100,default='Hi')
-     Answer1= models.CharField(max_length=100,default='Hi')
-     # Answer2= models.CharField(max_length=200,default='Hi')
-     # Answer3= models.CharField(max_length=200,default='Hi')
+    client= models.CharField(max_length=100,default='Hi')
+    Answer1= models.CharField(max_length=100,default='Hi')
+    Answer2= models.CharField(max_length=200,default='Hi')
+    Answer3= models.CharField(max_length=200,default='Hi')
     
